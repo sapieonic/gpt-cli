a2389be2fad1ebd5483240bd580a6825123b9159
Manas Ranjan <manasnilarout@gmail.com>
2024-09-01 14:27:45 +0530

    feat: Adds version release logic to CI pipeline (#2)
    Co-authored-by: manasnilorout <manas.nilorout@uipath.com>

--- src/gpts/IGptBaseClass.js
+++ src/gpts/IGptBaseClass.js
@@ -3,6 +3,26 @@
  */
 class IGptBaseClass {
   /**
+   * @param {Array} messages
+   * @param {string} question
+   * @returns {string}
+   * @throws {Error}
+   */
+  constructMessages(messages, query) {
+    throw new Error('Method not implemented.');
+  }
+
+  /**
+   * @param {Array} messages
+   * @param {string} assistantResponse
+   * @returns {string}
+   * @throws {Error}
+   */
+  addAssistantResponse(messages, assistantResponse) {
+    throw new Error('Method not implemented.');
+  }
+
+  /**
    * @param {string} question
    * @returns {Promise<string>}
    */
